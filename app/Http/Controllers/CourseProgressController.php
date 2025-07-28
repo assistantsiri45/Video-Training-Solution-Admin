3dc82a1b045f71ae551e1267fb6b72f05921ec1b
Ashish Chaurasiya <141113497+ashishh14@users.noreply.github.com>
2025-07-28 11:13:38 +0530

    cheng time
    cheng time

--- app/Http/Controllers/CourseProgressController.php
+++ app/Http/Controllers/CourseProgressController.php
@@ -7,33 +7,6 @@ use Illuminate\Http\Request;
 use App\Models\CourseProgress;
 class CourseProgressController extends Controller
 {
-// public function save(Request $request)
-//     {
-//      //  dd("hello");
-//         $request->validate([
-//             'course_id' => 'nullable',
-//             'progress_percent' => 'required|integer|min:0|max:100',
-//     'cmi_core_lesson_location' => 'nullable|string',
-//     'cmi_core_lesson_status' => 'nullable|string',
-//         ]);
-//         $userId = auth()->id();
-//  // dd($request->all());
-//         $progress = AppCourseProgress::updateOrCreate(
-//             [
-//                 'user_id' => $userId,
-//                  'course_id' => $request->course_id
-//             ],
-//             [
-//                 'progress_percent' => $request->progress_percent,
-//         'cmi_core_lesson_location' => $request->cmi_core_lesson_location,
-//         'cmi_core_lesson_status' => $request->cmi_core_lesson_status,
-//             ]
-//         );
-//         // dd("heloo45");
-//     //   dd($progress->all());
-
-//         return response()->json(['status' => 'success', 'message' => 'Progress saved']);
-//     }
 
 public function save(Request $request)
 {
@@ -51,7 +24,6 @@ public function save(Request $request)
         'user_id' => $userId,
         'course_id' => $request->course_id,
     ]);
-
     // Add session time to existing one (if any)
     $oldTime = $progress->session_time ?? 0;
     $newTime = $request->session_time ?? 0;
@@ -62,7 +34,6 @@ public function save(Request $request)
     if ($totalSessionTime >= 600) {
         $lessonStatus = 'completed';
     }
-
     // Update or insert progress
     $progress->session_time = $totalSessionTime;
     $progress->cmi_core_lesson_location = $request->cmi_core_lesson_location ?? $progress->cmi_core_lesson_location;
