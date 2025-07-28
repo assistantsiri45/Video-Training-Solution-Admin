5614855fea3fde7f623934c3ab92bcc721454457
Ashish Chaurasiya <141113497+ashishh14@users.noreply.github.com>
2025-07-28 10:51:00 +0530

    add time url add
    time add url add save database

--- app/Http/Controllers/CourseProgressController.php
+++ app/Http/Controllers/CourseProgressController.php
@@ -7,34 +7,6 @@ use Illuminate\Http\Request;
 use App\Models\CourseProgress;
 class CourseProgressController extends Controller
 {
-// public function save(Request $request)
-//     {
-//      //  dd("hello");
-//         $request->validate([
-//             'course_id' => 'nullable',
-//             'progress_percent' => 'required|integer|min:0|max:100',
-//     'cmi_core_lesson_location' => 'nullable|string',
-//     'cmi_core_lesson_status' => 'nullable|string',
-//         ]);
-//         $userId = auth()->id();
-//  // dd($request->all());
-//         $progress = AppCourseProgress::updateOrCreate(
-//             [
-//                 'user_id' => $userId,
-//                  'course_id' => $request->course_id
-//             ],
-//             [
-//                 'progress_percent' => $request->progress_percent,
-//         'cmi_core_lesson_location' => $request->cmi_core_lesson_location,
-//         'cmi_core_lesson_status' => $request->cmi_core_lesson_status,
-//             ]
-//         );
-//         // dd("heloo45");
-//     //   dd($progress->all());
-
-//         return response()->json(['status' => 'success', 'message' => 'Progress saved']);
-//     }
-
 public function save(Request $request)
 {
     $request->validate([
